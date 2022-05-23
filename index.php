1300d511f10b0a387523229b577b6ac686a02908
chunga-changa2 <52421151+chunga-changa2@users.noreply.github.com>
2022-05-23 19:00:18 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../build/css/style.css">
+    <title>Safezone</title>
+</head>
+<body>
+    <?php include "./blocks/settingsfile.php";?>
+    <?php include "blocks/header.php";?>
+    <?php include "blocks/bigimg.php";?>
+    <?php include "blocks/test.php";?>
+    <?php include "blocks/our-services.php";?>
+    <?php include "blocks/about-us.php";?>
+    <?php include "blocks/lid-box.php";?>
+    <?php include "blocks/feedback-cust.php";?>
+    <?php include "blocks/licenses.php";?>
+    <?php include "blocks/map.php";?>
+    <?php include "blocks/footer.php";?>
+    <!--  -->
+    <?php include "blocks/callback-form.php";?>
+    <?php // include "blocks/send-example.php";?>
+    <!--  -->
+    <script src="../build/js/script.js"></script>
+    <script src="jquery.maskedinput.min.js"></script>
+    <!-- <script src="hystmodal.min.js"></script> -->
+    <!-- <script></script> -->
+</body>
+</html>
