0a209a38c7d3c9db5fb9ddb82498de78fb368ac6
chunga-changa2 <52421151+chunga-changa2@users.noreply.github.com>
2022-05-23 19:03:17 -0400

    Create about-us.php

---
+++ blocks/about-us.php
@@ -0,0 +1,72 @@
+<div class="about-us-wrapp">
+    <div class="head-text-box">
+        <p>About us</p>
+    </div>
+    <div class="text-about-us">
+        <p>Safezone is one of the leading New York digital security systems company offering end-to-end access control, security camera, intercom, security alarm, networking systems as well as wiring and pipe conduit services throughout all five NYC boroughs (Manhattan, Queens, Brooklyn, the Bronx and Staten Island) also serving states of New Jersey, Pennsylvania and Connecticut. <br> We specialize in planning, custom design, installation, repair, configuration, integration and maintenance of digital security systems everywhere you need it, from homes and multi-family residential buildings to retail, businesses and offices, commercial spaces and hospitality, healthcare, governmental and educational facilities. <br> Providing expert advice we’ll go through the options that will work best for you and help you prepare a security plan tailored to your location. <br> SAFEZONE – Insured. Licensed. Experienced. Compliant.</p>
+    </div>
+    <div class="box-about-us-wrapp">
+        <div class="box-about-us">
+            <div class="img-box-about-us">
+                <div class="bg-img-box-about-us">
+                    <svg viewBox="0 0 24 24" fill="none" class="svg-img-about-us-wrapp">
+                    <path d="M9 11C11.2091 11 13 9.20914 13 7C13 4.79086 11.2091 3 9 3C6.79086 3 5 4.79086 5 7C5 9.20914 6.79086 11 9 11Z" stroke="#1A202C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M3 21V19C3 17.9391 3.42143 16.9217 4.17157 16.1716C4.92172 15.4214 5.93913 15 7 15H11C12.0609 15 13.0783 15.4214 13.8284 16.1716C14.5786 16.9217 15 17.9391 15 19V21" stroke="#1A202C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M16 3.13C16.8604 3.35031 17.623 3.85071 18.1676 4.55232C18.7122 5.25392 19.0078 6.11683 19.0078 7.005C19.0078 7.89318 18.7122 8.75608 18.1676 9.45769C17.623 10.1593 16.8604 10.6597 16 10.88" stroke="#1A202C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M21 21V19C20.9949 18.1172 20.6979 17.2608 20.1553 16.5644C19.6126 15.868 18.8548 15.3707 18 15.15" stroke="#1A202C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                </div>
+            </div>
+            <p class="text-box-about-us">Over 300 satisfied customers</p>
+        </div>
+        <div class="box-about-us">
+        <div class="img-box-about-us">
+            <div class="bg-img-box-about-us">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="svg-img-about-us-wrapp">
+                <path d="M18 5H6C4.89543 5 4 5.89543 4 7V19C4 20.1046 4.89543 21 6 21H18C19.1046 21 20 20.1046 20 19V7C20 5.89543 19.1046 5 18 5Z" stroke="#1A202C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M16 3V7" stroke="#1A202C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M8 3V7" stroke="#1A202C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M4 11H20" stroke="#1A202C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M11 15H12" stroke="#1A202C" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M12 15V18" stroke="#1A202C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            </div>
+        </div>
+        <p class="text-box-about-us">6 years of successful work</p>
+        </div>
+        <div class="box-about-us">
+        <div class="img-box-about-us">
+            <div class="bg-img-box-about-us">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="svg-img-about-us-wrapp">
+                <path d="M17 11H7C5.89543 11 5 11.8954 5 13V19C5 20.1046 5.89543 21 7 21H17C18.1046 21 19 20.1046 19 19V13C19 11.8954 18.1046 11 17 11Z" stroke="#1A202C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M12 17C12.5523 17 13 16.5523 13 16C13 15.4477 12.5523 15 12 15C11.4477 15 11 15.4477 11 16C11 16.5523 11.4477 17 12 17Z" stroke="#1A202C" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M8 11V7C8 5.93913 8.42143 4.92172 9.17157 4.17157C9.92172 3.42143 10.9391 3 12 3C13.0609 3 14.0783 3.42143 14.8284 4.17157C15.5786 4.92172 16 5.93913 16 7V11" stroke="#1A202C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+
+            </div>
+        </div>
+        <p class="text-box-about-us">Full range of turnkey services</p>
+        </div>
+        <div class="box-about-us">
+        <div class="img-box-about-us">
+            <div class="bg-img-box-about-us">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="svg-img-about-us-wrapp"><g id="Bookmark"><g id="Bookmark_2">
+                <path id="Stroke 1" fill-rule="evenodd" clip-rule="evenodd" d="M19.7388 6.15362C19.7388 3.40274 17.8581 2.29999 15.1503 2.29999H8.79143C6.16687 2.29999 4.19995 3.32756 4.19995 5.97016V20.694C4.19995 21.4198 4.9809 21.8769 5.61348 21.522L11.9954 17.9421L18.3223 21.516C18.9558 21.8729 19.7388 21.4158 19.7388 20.689V6.15362Z" stroke="#1A202C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            </div>
+        </div>
+        <p class="text-box-about-us">3 years warranty</p>
+        </div>
+        <div class="box-about-us">
+        <div class="img-box-about-us">
+            <div class="bg-img-box-about-us">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="svg-img-about-us-wrapp"><g id="Filter"><g id="Filter_2"><path id="Stroke 1" d="M10.3301 16.5929H4.02942" stroke="#1A202C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/><path id="Stroke 3" d="M13.1405 6.90036H19.4412" stroke="#1A202C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                <path id="Stroke 5" fill-rule="evenodd" clip-rule="evenodd" d="M8.72629 6.84625C8.72629 5.5506 7.66813 4.5 6.36314 4.5C5.05816 4.5 4 5.5506 4 6.84625C4 8.14191 5.05816 9.19251 6.36314 9.19251C7.66813 9.19251 8.72629 8.14191 8.72629 6.84625Z" stroke="#1A202C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                <path id="Stroke 7" fill-rule="evenodd" clip-rule="evenodd" d="M20 16.5538C20 15.2581 18.9426 14.2075 17.6376 14.2075C16.3318 14.2075 15.2737 15.2581 15.2737 16.5538C15.2737 17.8494 16.3318 18.9 17.6376 18.9C18.9426 18.9 20 17.8494 20 16.5538Z" stroke="#1A202C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            </div>
+        </div>
+        <p class="text-box-about-us">Rates for any budget</p>
+        </div>
+    </div>
+</div>
